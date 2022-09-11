80670497d8220525caae8c82251b396a1908a0ec
juanpablo <juan2224649@correo.uis.edu.co>
2022-09-11 22:03:04 +0200

    Se agrega el juego

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Piedra_papel_tijera
+
+# Analisis
+Opciones para elegir:piedra papel y tijera
+El usuario debe tener un menu para elegir las tres opciones 
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ piedra_papel_tijera.py
@@ -0,0 +1,83 @@
+# Ejercicio de juego : Piedra, papel y tijera
+import random
+print("----------------------------------")
+print("-------PIEDRA-PAPEL-TIJERA--------")
+print("----------------------------------")
+
+print("+________________________________+")
+print("|       Bienvenido jugador       |")
+print("|                                |")
+print("|   Vas a luchar contra windows  |")
+print("|     _______________________    |")
+print("|     |     _          _     |   |")
+print("|     |     O          O     |   |")
+print("|     |           V          |   |")
+print("|     |         /////        |   |")
+print("|     |______________________|   |")
+print("+________________________________+")
+
+# intput
+bandera = False
+
+print("\n¡Elija una opcion para atacar!\n")
+#se despliega el menu para seleccionar la opcion : 
+print("1. Piedra")
+print("2. Papel")
+print("3. tijera")
+
+
+usuario = int(input("\nAtaque: "))
+
+win = random.randint(1,3)
+
+#procesing
+if(usuario == win ):
+    print("-----------------------")
+    print("--------EMPATE---------")
+    print("-----------------------")
+
+elif (usuario ==1 and win == 2):
+
+   
+    r = print("Elegistes piedra y Windows papel")
+    r = print("--------PERDISTE---------")
+   
+
+elif (usuario ==1 and win == 3):
+    r = print("Elegistes piedra y Windows tijeras")
+    r = print("--------VICTORIA---------")
+
+elif (usuario ==2 and win == 3):
+    r = print("Elegistes papel y Windows tijeras")
+    r = print("--------PERDISTE---------")
+
+elif (usuario ==2 and win == 1):
+    r = print("Elegistes papel y Windows piedra")
+    r = print("--------VICTORIA---------")
+
+elif (usuario ==3 and win == 1):
+    r = print("Elegistes tijeras y Windows piedra")
+    r = print("--------PERDISTE---------")
+
+elif (usuario ==3 and win == 2):
+    r = print("Elegistes tijeras y Windows papel")
+    r = print("--------VICTORIA---------")
+
+    
+    #print("         ___             ")
+    #print("_________|  |__________")
+    #print("             __________|")
+    #print("             __________|")
+    #print("          _____________|")
+    #print("___________________|")
+
+   
+    
+    
+
+
+
+
+    
+
+    
